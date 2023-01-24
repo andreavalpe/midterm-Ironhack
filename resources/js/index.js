2df3284f867c4c3be233966782608c204709a2b1
Andrea <you@example.com>
2023-01-24 09:07:19 +0100

    modificando roadmap

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,8 @@
 /* Crea tu propia lógica para hacer fetch de 3 posts distintos y enseñarlos en la homepage con DOM manipulation */
+// function  cambiarSeccion (){
+//     fetch ("project.html")
+//     .then (response => response.text ())
+//     .then (data => {
+//         document.getElementById ("section1").innerHTML= data; 
+//     });
+// }
